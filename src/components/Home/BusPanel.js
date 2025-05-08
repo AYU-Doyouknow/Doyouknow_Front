1bd0d9005b9c5a63aafe45d6b9a5a74d44f483f3
shuyomi <nicole030312@naver.com>
2025-05-08 15:49:01 +0900

    Feat 날씨/버스 컴포넌트 초기작업

---
+++ src/components/Home/BusPanel.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../styles/Home/BusPanel.css";
+
+const BusPanel = () => {
+
+    return(
+        <div className="bus">
+        버스
+        </div>
+
+
+);
+};
+
+export default BusPanel;
---
+++ src/components/Home/WeatherPanel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../styles/Home/WeatherPanel.css";
+
+const WeatherPanel = () => {
+
+    return(
+        <div className="Weather">
+        날씨
+    </div>
+
+);
+};
+
+export default WeatherPanel;
